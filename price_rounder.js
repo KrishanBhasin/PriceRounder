f098ff6f6932f823306d3e9c4b9868a33eaa6849
Krishan Bhasin <KrishanBhasin@users.noreply.github.com>
2017-12-17 20:30:34 +0000

    move function definitions outside handleText

--- price_rounder.js
+++ price_rounder.js
@@ -27,32 +27,27 @@ function walk(node)
 	}
 }
 
+function roundUpMoney(inputString, units){
+    var unroundedVal = inputString.substring(1);
+    var returnVal = Math.ceil(parseFloat(unroundedVal)).toString();
+    return units + returnVal + ".00";
+}
+
+function roundUpMoneyPounds(inputString){
+    return roundUpMoney(inputString, "£");
+}
+
+function roundUpMoneyDollars(inputString){
+    return roundUpMoney(inputString, "$");
+}
+
 function handleText(textNode) 
 {
     var v = textNode.nodeValue;
 
-    var re = /£[0-9]*\.99/g;
-    
-    function roundUpMoney(inputString, units){
-        var unroundedVal = inputString.substring(1);
-        var returnVal = Math.ceil(parseFloat(unroundedVal)).toString();
-        return units + returnVal + ".00";
-    }
-
-    function roundUpMoneyPounds(inputString){
-        return roundUpMoney(inputString, "£");
-    }
-    
-    function roundUpMoneyDollars(inputString){
-        return roundUpMoney(inputString, "$");
-    }
-
     v = v.replace(/£[0-9]*\.99/g, roundUpMoneyPounds)
-
     v = v.replace(/\$[0-9]*\.99/g, roundUpMoneyDollars)
-    
-    
-	
+    	
 	textNode.nodeValue = v;
 }
 
